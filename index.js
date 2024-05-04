8ceccf869b273406f4a4da72943fb3c9ece42e8f
GuiTavs7 <7tavs7@gmail.com>
2024-05-04 15:26:13 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Hello World!")
+
+console.log("Oii!")
+
+console.log("Dizendo oi do console!")
+
+
+//COMENTÁRIOS EM JS
+
+/*
+
+comentando o bloco do código
+
+*/
+
+
+// comentando apenas a linha do código
